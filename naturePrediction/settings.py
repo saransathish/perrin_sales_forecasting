7268c5d1c371c0156f9b515181883d81de3ec93d
saran <saransathi1010@gmail.com>
2024-07-08 11:33:08 +0530

    adding startic files

--- naturePrediction/settings.py
+++ naturePrediction/settings.py
@@ -107,6 +107,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
